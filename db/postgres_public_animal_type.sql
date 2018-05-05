d93b5249c5e51820d9f4ed66f59d323f5e76abd3
OK <kalashnikovas@me.com>
2018-05-05 03:33:00 +0300

    lesson9

--- db/postgres_public_animal_type.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.animal_type (id, name) VALUES (1, 'Cat');
-INSERT INTO public.animal_type (id, name) VALUES (2, 'Dog');
-INSERT INTO public.animal_type (id, name) VALUES (3, 'Fish');
